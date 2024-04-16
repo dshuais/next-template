74c2ee974d835ea6d947209c6d96957e007e66fd
dshuais <1137896420@qq.com>
2024-04-16 14:31:47 +0800

    ✨ feat: 新增loading组件

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -2,12 +2,12 @@
  * @Author: dushuai
  * @Date: 2024-04-16 11:14:03
  * @LastEditors: dushuai
- * @LastEditTime: 2024-04-16 11:38:49
- * @description: 心平气和
+ * @LastEditTime: 2024-04-16 14:05:39
+ * @description: globalError
  */
 "use client"
 
-export default function error() {
+export default function globalError() {
   return (
     <div>error</div>
   )
---
+++ src/app/loading.tsx
@@ -0,0 +1,38 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-03-29 18:14:56
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-04-16 14:25:00
+ * @description: loading 组件
+ */
+import styles from "./loading.module.css"
+
+export default function Loading() {
+
+  return (
+    <LoadingTwo />
+  )
+}
+
+// 规则循环旋转
+function LoadingOne() {
+  return (
+    <div className={styles.loading}>
+      <div className={styles.loader}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+      </div>
+    </div>
+  )
+}
+
+// 不规则旋转
+function LoadingTwo() {
+  return (
+    <div className={styles['loading-two']}>
+      <div className={styles['loader-two']}></div>
+    </div>
+  )
+}
