ef76591440bb4e44a28527f7ab82ca28565163b6
dshuais <1137896420@qq.com>
2024-04-16 18:08:12 +0800

    ✨ feat: add error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,32 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-04-15 17:10:01
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-04-16 17:39:35
+ * @description: Error
+ */
+'use client' // Error components must be Client Components
+
+import { useEffect } from 'react'
+
+export default function Error({ error, reset, }: { error: Error & { digest?: string }, reset: () => void }) {
+
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
