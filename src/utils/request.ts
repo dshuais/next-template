383cac216c6794fd3a704cd584f5815a6c66d75b
dushuais <1137896420@qq.com>
2024-04-20 13:08:31 +0800

    ✨ feat: 封装fetch request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,10 +2,36 @@
  * @Author: dushuai
  * @Date: 2024-04-19 16:29:03
  * @LastEditors: dushuai
- * @LastEditTime: 2024-04-19 17:44:47
+ * @LastEditTime: 2024-04-20 13:07:40
  * @description: 封装request
  */
 
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
 
+/**
+ * 封装request fetch
+ */
+class NextRequest {
 
+  /**
+   * 请求拦截
+   */
+  interceptorsRequest() {
+
+  }
+
+  /**
+   * 响应拦截
+   */
+  interceptorsResponse() {
+
+  }
+
+}
+
+/**
+ * 自定义fetch
+ */
+const request = new NextRequest()
+
+export default request
